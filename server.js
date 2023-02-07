8ae98e8750197db6b1f47cd21a554dcc74375c72
Dmytro2V <op2vin@gmail.com>
2023-02-07 22:53:18 +0200

    done

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const http = require('http');
+const { runInNewContext } = require('vm');
 
 let nextDogId = 1;
 
@@ -32,8 +33,54 @@ const server = http.createServer((req, res) => {
       console.log(req.body);
     }
     // Do not edit above this line
+    
 
     // define route handlers here
+    if (req.method === 'GET' && req.url === '/') {
+      res.statusCode = 200;
+      res.setHeader('Content-Type','text/plain');
+      res.body = 'Dog Clubs';
+      return res.end(res.body);
+    };
+    if (req.method === 'GET' && req.url === '/dogs') {
+      res.statusCode = 200;
+      res.setHeader('Content-Type','text/plain');
+      //res.body = 'Dogs index';
+      return res.end('Dogs index')
+    };
+   // bonus Endpoints tasks:
+   let url = req.url
+   let urlParts = url.split('/')
+   if (req.method === 'GET' && url.startsWith('/dogs/new')) {
+    res.statusCode = 200;
+    res.setHeader('Content-Type','text/plain');
+    res.body = 'Dog create from page';
+    return res.end(res.body)
+  }; 
+  if (req.method === 'GET' && url.startsWith('/dogs/') && urlParts.length === 3 && urlParts[3]) {
+    res.statusCode = 200;
+    res.setHeader('Content-Type','text/plain');
+    res.body = 'Dog edit from page for dogId:' + urlParts[3];
+    return res.end(res.body)
+  };
+   if (req.method === 'GET' && url.startsWith('/dogs/') && urlParts[2]) {
+    res.statusCode = 200;
+    res.setHeader('Content-Type','text/plain');
+    res.body = 'Dog details for dogid:' + urlParts[2];
+    return res.end(res.body)
+  };
+  if (req.method === 'POST' && url ==='/dogs') {
+    res.statusCode = 302;    
+    res.setHeader('Location','/dogs/' + getNewDogId()) ;
+    return res.end()
+  };
+  if (req.method === 'POST' && url.startsWith('/dogs/') && urlParts[2]) {
+    res.statusCode = 302;     
+    res.setHeader( 'Location','/dogs/' + urlParts[2]) ;
+    return res.end()
+  };
+
+
 
     // Do not edit below this line
     // Return a 404 response when there is no matching route handler
